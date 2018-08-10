d273c98be58abd925299a7d727919757d52001a6
DJOFFF <myemail@myemail.com>
2018-08-09 22:13:30 -0400

    Split Stats tab + create controler

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -45,26 +45,6 @@ public class Controller {
     @FXML
     private Button addToDB;
 
-    @FXML
-    private PieChart partiesJoues;
-    @FXML
-    private PieChart manchesJoues;
-    @FXML
-    private PieChart pointsJoues;
-
-    @FXML
-    private LineChart deltaPoints;
-    @FXML
-    private LineChart deltaManches;
-    @FXML
-    private LineChart deltaGames;
-    @FXML
-    private BarChart blanchissage;
-
-    @FXML
-    private Button stats_get_data;
-
-
     public Controller(){
         playerData.add(new Person("Joffrey"));
         playerData.add(new Person("JPB"));
@@ -171,53 +151,6 @@ public class Controller {
         player2.getSelectionModel().select(0);
         selectedPerson1 = player2.getSelectionModel().getSelectedItem();
         pointsTable_col2.setText(selectedPerson1.getLastName());
-
-        stats_get_data.setOnAction((event)-> {
-            SQL_ctrler sc = new SQL_ctrler();
-            GameStats gs = new GameStats();
-            String pl1 = player1.getSelectionModel().getSelectedItem().getLastName();
-            String pl2 = player2.getSelectionModel().getSelectedItem().getLastName();
-            gs = sc.getGame(pl1, pl2);
-            int p1m = gs.getPlayer1_manches();
-            int p2m = gs.getPlayer2_manches();
-            int p1p = gs.getPlayer1_parties();
-            int p2p = gs.getPlayer2_parties();
-            int np = gs.getParties_nulles();
-            int p1pts = gs.getPlayer1_points();
-            int p2pts = gs.getPlayer2_points();
-
-            ObservableList<PieChart.Data> pieChartData =
-                    FXCollections.observableArrayList(
-                            new PieChart.Data(pl1 + " " + Integer.toString(p1p), p1p),
-                            new PieChart.Data(pl2 + " " + Integer.toString(p2p), p2p),
-                            new PieChart.Data("nulles " + Integer.toString(np), np));
-            partiesJoues.setData(pieChartData);
-            //manchesJoues
-            ObservableList<PieChart.Data> pieChartData2 =
-                    FXCollections.observableArrayList(
-                            new PieChart.Data(pl1 + " " + Integer.toString(p1m), p1m),
-                            new PieChart.Data(pl2 + " " + Integer.toString(p2m), p2m));
-            manchesJoues.setData(pieChartData2);
-            ObservableList<PieChart.Data> pieChartData3 =
-                    FXCollections.observableArrayList(
-                            new PieChart.Data(pl1 + " " + Integer.toString(p1pts), p1pts),
-                            new PieChart.Data(pl2 + " " + Integer.toString(p2pts), p2pts));
-            pointsJoues.setData(pieChartData3);
-            XYChart.Series<String, Integer> series = gs.getDeltapoints();
-            series.setName("dPoints");
-            deltaPoints.getData().add(series);
-            XYChart.Series<String, Integer> series2 = gs.getDeltagames();
-            series2.setName("dGames");
-            deltaGames.getData().add(series2);
-            XYChart.Series<String, Integer> series3 = gs.getDeltamanches();
-            series3.setName("dManches");
-            deltaManches.getData().add(series3);
-            XYChart.Series<String, Integer> series4 = new XYChart.Series<>();
-            series4.setName("Blanchissage");
-            series4.getData().add(new XYChart.Data<>(pl1, gs.getPlayer1_blanchissage()));
-            series4.getData().add(new XYChart.Data<>(pl2, gs.getPlayer2_blanchissage()));
-            blanchissage.getData().add(series4);
-        });
 
         addToDB.setOnAction((event) -> {
             SQL_ctrler sql_ctrler = new SQL_ctrler();
---
+++ src/sample/StatsController.java
@@ -0,0 +1,92 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+
+public class StatsController {
+
+
+    private ObservableList<Person> playerData = FXCollections.observableArrayList();
+
+    @FXML
+    private PieChart partiesJoues;
+    @FXML
+    private PieChart manchesJoues;
+    @FXML
+    private PieChart pointsJoues;
+
+    @FXML
+    private LineChart deltaPoints;
+    @FXML
+    private LineChart deltaManches;
+    @FXML
+    private LineChart deltaGames;
+    @FXML
+    private BarChart blanchissage;
+
+    @FXML
+    private Button stats_get_data;
+
+
+    public StatsController(){
+    }
+
+    @FXML
+    private void initialize(){
+
+        
+        stats_get_data.setOnAction((event)-> {
+            SQL_ctrler sc = new SQL_ctrler();
+            GameStats gs = new GameStats();
+            String pl1 = "test";
+            String pl2 = "test";
+            gs = sc.getGame(pl1, pl2);
+            int p1m = gs.getPlayer1_manches();
+            int p2m = gs.getPlayer2_manches();
+            int p1p = gs.getPlayer1_parties();
+            int p2p = gs.getPlayer2_parties();
+            int np = gs.getParties_nulles();
+            int p1pts = gs.getPlayer1_points();
+            int p2pts = gs.getPlayer2_points();
+
+            ObservableList<PieChart.Data> pieChartData =
+                    FXCollections.observableArrayList(
+                            new PieChart.Data(pl1 + " " + Integer.toString(p1p), p1p),
+                            new PieChart.Data(pl2 + " " + Integer.toString(p2p), p2p),
+                            new PieChart.Data("nulles " + Integer.toString(np), np));
+            partiesJoues.setData(pieChartData);
+            //manchesJoues
+            ObservableList<PieChart.Data> pieChartData2 =
+                    FXCollections.observableArrayList(
+                            new PieChart.Data(pl1 + " " + Integer.toString(p1m), p1m),
+                            new PieChart.Data(pl2 + " " + Integer.toString(p2m), p2m));
+            manchesJoues.setData(pieChartData2);
+            ObservableList<PieChart.Data> pieChartData3 =
+                    FXCollections.observableArrayList(
+                            new PieChart.Data(pl1 + " " + Integer.toString(p1pts), p1pts),
+                            new PieChart.Data(pl2 + " " + Integer.toString(p2pts), p2pts));
+            pointsJoues.setData(pieChartData3);
+            XYChart.Series<String, Integer> series = gs.getDeltapoints();
+            series.setName("dPoints");
+            deltaPoints.getData().add(series);
+            XYChart.Series<String, Integer> series2 = gs.getDeltagames();
+            series2.setName("dGames");
+            deltaGames.getData().add(series2);
+            XYChart.Series<String, Integer> series3 = gs.getDeltamanches();
+            series3.setName("dManches");
+            deltaManches.getData().add(series3);
+            XYChart.Series<String, Integer> series4 = new XYChart.Series<>();
+            series4.setName("Blanchissage");
+            series4.getData().add(new XYChart.Data<>(pl1, gs.getPlayer1_blanchissage()));
+            series4.getData().add(new XYChart.Data<>(pl2, gs.getPlayer2_blanchissage()));
+            blanchissage.getData().add(series4);
+        });
+    }
+
+}
